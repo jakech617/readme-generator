2a5a1532563ce0bf81519bfedfb9537f686aeb0d
Jake <jakech617@gmail.com>
2021-03-27 14:17:50 -0400

    pushing working code

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# readme-generator
+
+        
+# README Generator
+
+## Table of Contents
+* [1. Description](#description)
+* [2. Installation](#installation)
+* [3. Usage](#usage)
+* [4. License](#license)
+* [5. Contributions](#contributions)
+* [6. Tests](#tests)
+
+## Description
+Generate a high quality README.md for your GitHub repo.
+
+## Installation
+Clone this repo to your local computer.
+
+## Usage
+Generates high quality README.md file for your projects.
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Contributions
+test
+
+## Tests
+tests 
--- index.js
+++ index.js
@@ -1,13 +1,109 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const init = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your project title?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What is your project description?',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Project installation instructions..',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Describe the usage for the project..',
+            name: 'usage'
+        },
+        {
+            type: 'list',
+            message: 'Please enter the license used',
+            name: 'license',
+            choices: [
+                'Apache License 2.0',
+                'BSD 3-Clause',
+                'GNU (GPL v3)',
+                'MIT',
+            ],
+                filter: (val) => { switch(val) {
+                
+                    case 'Apache License 2.0': return ['[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'];
+                    case 'BSD 3-Clause': return ['[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)' ]
+                    case 'GNU (GPL v3)': return ['[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)'];
+                    case 'MIT': return ['[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'];
+                    default: return [val, '#license']; 
+                };
+            },
+        },
+        {
+            type: 'input',
+            message: 'Enter any contributing information..',
+            name: 'contributing'
+        },
+        {
+            type: 'input',
+            message: 'Enter any tests..',
+            name: 'tests'
+        },
+        {
+            type: 'input',
+            message: 'Please enter your GitHub username',
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: 'Please enter your Email',
+            name: 'email',
+        }
+    
+]).then ((answers) => {
+        
+const readMeGen = `
+        
+# ${answers.title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Table of Contents
+* [1. Description](#description)
+* [2. Installation](#installation)
+* [3. Usage](#usage)
+* [4. License](#license)
+* [5. Contributions](#contributions)
+* [6. Tests](#tests)
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${answers.description}
 
-// Function call to initialize app
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributions
+${answers.contributing}
+
+## Tests
+${answers.tests} 
+`
+    fs.writeFile('./README.md', readMeGen, (err) => {
+        if (err) throw err
+        else {
+            console.log('Successfully wrote file to README.md')
+        }
+    });
+});
+
+}
+
 init();
